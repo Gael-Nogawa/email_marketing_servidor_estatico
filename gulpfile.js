2fb25ff7c1a0839c4314b92700a18f3d10045dd8
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-07-21 01:43:56 -0300

    Chore: mundando o caminho

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const imagemin = require('gulp-imagemin');
 function comprimeImagens(){
     return gulp.src('./src/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/images/*'));
 }
 
 exports.images = comprimeImagens;
